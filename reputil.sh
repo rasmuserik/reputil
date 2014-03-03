e66e2450ddca8ac5656ed01fcf1ceea5938a4433
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-03-03 20:49:44 +0100

    extra search path

--- reputil.sh
+++ reputil.sh
@@ -2,6 +2,8 @@
 dir=`dirname $0`
 if [ -e $dir/reputil.js ]; then
   /usr/bin/env node $dir/reputil.js "$@"
+elif [ -e $dir/../reputil/reputil.js ]; then
+  /usr/bin/env node $dir/../reputil/reputil.js "$@"
 else
   /usr/bin/env node $dir/../lib/node_modules/reputil/reputil.js "$@"
 fi
