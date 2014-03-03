4b380c2a6e8456b81be0a5de9336e98aa426cbfc
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-03-03 15:35:00 +0100

    support for generation of phonegap config.xml

---
+++ reputil.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+dir=`dirname $0`
+if [ -e $dir/reputil.js ]; then
+  /usr/bin/env node $dir/reputil.js "$@"
+else
+  /usr/bin/env node $dir/../lib/node_modules/reputil/reputil.js "$@"
+fi
